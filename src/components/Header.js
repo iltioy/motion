fbc4861f884cb4839cad17fcb671e145842c4fa4
iltioy <tema.illar2@gmail.com>
2022-10-18 23:59:41 +0300

    2

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,8 +22,9 @@ const Header = () => {
             </div>
             <motion.div
                 className="title"
-                animate={{ y: -10 }}
                 initial={{ y: -250 }}
+                animate={{ y: -10 }}
+                transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
             >
                 <h1>Pizza Joint</h1>
             </motion.div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,26 @@ import { motion } from "framer-motion";
 
 const Home = () => {
     return (
-        <motion.div className="home container" animate={{}}>
+        <motion.div
+            className="home container"
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{
+                delay: 1.5,
+                duration: 1.5,
+            }}
+        >
             <h2>Welcome to Pizza Joint</h2>
             <Link to="/base">
-                <motion.button animate={{}}>Create Your Pizza</motion.button>
+                <motion.button
+                    whileHover={{
+                        scale: 1.1,
+                        textShadow: "0px 0px 8px rgb(255, 255, 255)",
+                        boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+                    }}
+                >
+                    Create Your Pizza
+                </motion.button>
             </Link>
         </motion.div>
     );
